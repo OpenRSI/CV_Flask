154bedaec9c3e1c5527f89ce20be89f36b1abdf0
Emile <133158597+Elie00000@users.noreply.github.com>
2024-02-20 11:18:11 +0000

    kjhu

--- __init__.py
+++ __init__.py
@@ -1,13 +1,8 @@
-from cv import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify
 import json
 import sqlite3
 
 app = Flask(__name__) #creating flask app name
-
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
 
 @app.route('/')
 def home():
