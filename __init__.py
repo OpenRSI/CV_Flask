0d51d7ec643d318c986848250deac12359eb45d9
Emile <133158597+Elie00000@users.noreply.github.com>
2024-02-20 15:51:45 +0000

    Sur un malentendu ca peut marcher

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ def messages():
 
 @app.route("/consultation/")
 def ReadBDD():
-    conn = sqlite3.connect('/home/elie0000/www/cv/database.db')
+    conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM messages;')
     data = cursor.fetchall()
@@ -35,6 +35,30 @@ def Readfiche(post_id):
     
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
+
+@app.route('/messages/', methods=['GET', 'POST'])
+def ajouter_client():
+    if request.method == 'POST':
+        # Récupérer les données du formulaire
+        prenom = request.form['email']
+        adresse = request.form['message']
+
+        # Insérer les données dans la base de données (ici, je suppose que tu as une table 'clients')
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        if conn is not None:
+            cursor.execute('INSERT INTO messages (email, message) VALUES (?, ?)', (email, message))
+            conn.commit()
+            conn.close()
+        else:
+            return 'Erreur de connexion à la base de données'
+
+        # Rediriger vers la page de consultation des clients après l'ajout
+        return redirect(url_for('/'))
+
+    # Si la méthode est GET, simplement rendre le template du formulaire
+    return render_template('messages.html')
+
 
 if(__name__ == "__main__"):
     app.run()
