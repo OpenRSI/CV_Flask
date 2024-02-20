ee238cdb467766458c643cdb8e9eabe9b8743a7a
Emile <133158597+Elie00000@users.noreply.github.com>
2024-02-20 10:49:43 +0000

    On essaye de faire en sorte que ca marchjhe

--- __init__.py
+++ __init__.py
@@ -14,20 +14,21 @@ def resume_1():
 
 @app.route('/cv')
 def resume_2():
-    return render_template("resume_2.html")
+    return render_template("cv.html")
 
 @app.route('/resume_template')
 def resume_template():
     return render_template("resume_template.html")
+
 # Création d'une nouvelle route pour la lecture de la BDD
 @app.route('/lecture/')
 def ReadBDD():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM livres').fetchall()
+    posts = conn.execute('SELECT * FROM messages').fetchall()
     conn.close()
 
     # Convertit la liste de livre en un format JSON
-    json_posts = [{'id': post['id'], 'title': post['title'], 'content': post['auteur']} for post in posts]
+    json_posts = [{'id': post['id'], 'email': post['email'], 'message': post['message']} for post in posts]
 
     # Renvoie la réponse JSON
     return jsonify(posts=json_posts)
